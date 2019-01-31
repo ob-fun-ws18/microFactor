9517a2f58a0c207a0a9e6394944b33fb5317f223
Andreas Bergmaier <andreas.bergmaier@hm.edu>
2019-01-31 16:00:00 +0100

    use mtl ExceptT

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,10 @@ module Main where
 
 import Control.Applicative ((<|>))
 import Control.Monad (forM_, (>=>))
-import Control.Monad.IO.Class
-import Control.Monad.Trans.State
+-- import Control.Monad.IO.Class
+import Control.Monad.Except
+import Control.Monad.State
+import Data.Bifunctor
 import Data.List (intercalate)
 import Data.Map.Lazy (Map, empty, lookup, keys, foldMapWithKey)
 import Data.Maybe (listToMaybe)
@@ -22,13 +24,15 @@ main :: IO ()
 main = do
     setTitle "MicroFactor"
     arg <- listToMaybe <$> getArgs
-    evalStateT (maybe repl (run . return . Load . Just) arg) AppState { definitions = empty, dictPath = arg, thread = newThread }
+    evalStateT (maybe repl (runCmds . return . Load . Just) arg) AppState { definitions = empty, dictPath = arg, thread = newThread }
 
 data AppState = AppState
     { definitions :: Map String [MicroFactorInstruction ResolvedRef]
     , dictPath :: Maybe FilePath
     , thread :: Thread ResolvedRef
 }
+
+data AppErr = QuitRepl | ParseErr ParseError | forall s. Show s => MsgErr s
 
 repl :: StateT AppState IO ()
 repl = do
@@ -38,72 +42,78 @@ repl = do
         setSGR []
         hFlush stdout
         getLine
-    handleError (runParser commandParser () "" line) run
+    handleError (runParser commandParser () "" line) runCmds
+
+runCmds :: [Command] -> StateT AppState IO ()
+runCmds [] = repl
+runCmds (c:cs) = runExceptT (run c) >>= \case
+    Left QuitRepl -> return ()
+    Left (ParseErr err) -> putErrorMessage err >> repl
+    Left (MsgErr err) -> liftIO (putWarning $ show err) >> repl
+    Right () -> runCmds cs
 
 
-run :: [Command] -> StateT AppState IO ()
-run [] = repl
-run (Quit:_) = return ()
-run (Define id val:cmds) = gets definitions >>= \defs ->
-    handleError (define id val defs) \newDefs -> do
-        modify (\state -> state { definitions = newDefs })
-        liftIO $ putStrLn $ "Defined " ++ id
-        run cmds
-run (Evaluate exp:cmds) = gets definitions >>= \defs ->
-    handleError (resolve defs exp) \rval -> do
-        -- print rval
-        state <- get
-        let res = interpret rval $ thread state
-        put state { thread = interpreterThread res }
-        liftIO $ forM_ (interpreterOutput res) putStrLn
-        -- liftIO $ print $ interpreterThread res
-        case interpreterValue res of
-            Left e -> liftIO $ putWarning $ show e
-            Right _ -> return ()
-        liftIO $ putStrLn $ "{ " ++ (intercalate " { " $ fmap showValue $ reverse $ dataStack $ interpreterThread res)
-        run cmds
-run (ShowDef id:cmds) = do
+run :: Command -> ExceptT AppErr (StateT AppState IO) ()
+run Quit = throwError QuitRepl
+run (Define id val) = do
+    defs <- gets definitions
+    newDefs <- liftEither $ first ParseErr $ define id val defs
+    modify (\state -> state { definitions = newDefs })
+    liftIO $ putStrLn $ "Defined " ++ id
+run (Evaluate exp) = do
+    defs <- gets definitions
+    rval <- liftEither $ first ParseErr $ resolve defs exp
+    -- print rval
+    state <- get
+    let res = interpret rval $ thread state
+    put state { thread = interpreterThread res }
+    liftIO $ forM_ (interpreterOutput res) putStrLn
+    -- liftIO $ print $ interpreterThread res
+    liftEither $ first MsgErr $ interpreterValue res
+    liftIO $ putStrLn $ "{ " ++ (intercalate " { " $ fmap showValue $ reverse $ dataStack $ interpreterThread res)
+run (ShowDef id) = do
     userFns <- gets definitions
     liftIO $ putStrLn $ case lookup id userFns of
         Nothing -> id ++ " not found"
         Just ref -> ":" ++ id ++ " " ++ show ref ++ ";"
-    run cmds
-run (List:cmds) = do
+run List = do
     userFns <- gets definitions
     liftIO $ putStrLn $ unwords $ keys (builtinSymbols :: Map String (MicroFactorInstruction ResolvedRef))
     liftIO $ putStrLn $ unwords $ keys userFns
-    run cmds
-run (Load arg:cmds) = do
+run (Load arg) = do
+    path <- getFilePath arg
+    -- TODO: catch io exceptions
+    parseRes <- liftIO $ withFile path ReadMode \h -> do
+        hSetEncoding h utf8
+        text <- hGetContents h
+        return $! runParser dictionaryParser () path text
+    defines <- liftEither $ first MsgErr $ parseRes
+    scope <- gets definitions
+    -- let x = foldr (>=>) return $ map (uncurry define) ds
+    -- https://hackage.haskell.org/package/foldl-1.4.5/docs/Control-Foldl.html#t:EndoM
+    -- let x = appEndoM $ foldMap (EndoM . uncurry define) ds
+    newDefs <- liftEither $ first MsgErr $ (foldr (>=>) return $ map (uncurry define) defines) scope
+    modify (\state -> state { definitions = newDefs })
+    liftIO $ putStrLn "ok."
+run (Save arg) = do
+    path <- getFilePath arg
+    scope <- gets definitions
+    let text = foldMapWithKey (\name def -> ":"++name++" "++show def++";\n") scope
+    -- TODO: catch io exceptions
+    liftIO $ withFile path WriteMode \h -> do
+        hSetEncoding h utf8
+        hPutStr h text
+
+data NoFilepath = NoFilepath
+instance Show NoFilepath where
+    show NoFilepath = "no filepath given"
+
+getFilePath :: Maybe FilePath -> ExceptT AppErr (StateT AppState IO) FilePath
+getFilePath arg = do
     path <- gets dictPath
     case arg <|> path of
-        Nothing -> (liftIO $ putWarning "no filepath given") >> repl
-        Just p -> do
-            -- TODO: catch io exceptions
-            defines <- liftIO $ withFile p ReadMode \h -> do
-                hSetEncoding h utf8
-                text <- hGetContents h
-                return $! runParser dictionaryParser () p text
-            scope <- gets definitions
-            -- let x = foldr (>=>) return $ map (uncurry define) ds
-            -- https://hackage.haskell.org/package/foldl-1.4.5/docs/Control-Foldl.html#t:EndoM
-            -- let x = appEndoM $ foldMap (EndoM . uncurry define) ds
-            let newDefs = defines >>= (\ds -> (foldr (>=>) return $ map (uncurry define) ds) scope)
-            either (\err -> liftIO (putWarning (show err)) >> repl) (\newScope -> do
-                modify (\state -> state { definitions = newScope })
-                liftIO $ putStrLn "ok."
-                run cmds) newDefs
-run (Save arg:cmds) = do
-    path <- gets dictPath
-    case arg <|> path of
-        Nothing -> (liftIO $ putWarning "no filepath given") >> repl
-        Just p -> do
-            scope <- gets definitions
-            let text = foldMapWithKey (\name def -> ":"++name++" "++show def++";\n") scope
-            -- TODO: catch io exceptions
-            liftIO $ withFile p WriteMode \h -> do
-                hSetEncoding h utf8
-                hPutStr h text
-            run cmds
+        Nothing -> throwError $ MsgErr $ NoFilepath
+        Just p -> return p
 
 
 handleError :: Either ParseError a -> (a -> StateT AppState IO ()) -> StateT AppState IO ()
